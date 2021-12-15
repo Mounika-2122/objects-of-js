ec89832126b06114c87662bb683fdbc2fab4a552
Mounika <bojjamounika8@gmail.com>
2021-12-15 23:07:20 +0530

    objectscs

---
+++ objects.js
@@ -0,0 +1,13 @@
+var student ={
+    name:"vani" ,
+    Age:20,
+    marks: {
+      science:85,
+      maths:95,
+      physics:70
+      
+    }
+  }
+  console.log(student.Age)
+ console.log(student.marks.maths)
+ console.log(typeof student)
